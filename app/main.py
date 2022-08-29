800b698306d9f5fd2ceb7890d242c67d747816f8
Heechan Kang <weston0713@gmail.com>
2022-08-30 08:06:56 +0900

    220830

--- app/main.py
+++ app/main.py
@@ -168,3 +168,205 @@ def delete_users(user_id: int):
 # ):
 #     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
 #     return item
+
+
+
+# --------------------------------------------------
+
+# # from enum import Enum
+# from typing import List, Union
+# from fastapi import FastAPI, Request
+# from starlette.middleware.cors import CORSMiddleware
+# from fastapi.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+
+# from .database import session
+# from .models import UserTable, User
+
+# # class ModelName(str, Enum):
+# #     alexnet = "alexnet",
+# #     resnet = "resnet",
+# #     lenet = "lenet"
+
+# templates = Jinja2Templates(directory='templates')
+
+# app = FastAPI()
+
+# app.add_middleware(
+# 	CORSMiddleware,
+#  	allow_origins=["*"],
+#  	allow_credentials=True,
+#  	allow_methods=["*"],
+#  	allow_headers=["*"],
+# )
+
+
+# # --------------------------------------------------
+# @app.get("/users", response_class=HTMLResponse)
+# def read_users(request: Request):
+#     context = {}
+#     users = session.query(UserTable).all()
+#     context["users"] = users
+#     context["request"] = request
+    
+#     return templates.TemplateResponse("user_list.html", context)
+
+# @app.get("/users/{user_id}", response_class=HTMLResponse)
+# def read_user(request: Request, user_id: int):
+#     context = {}
+#     user = session.query(UserTable).filter(UserTable.id == user_id).first()
+#     context["name"] = user.name
+#     context["age"] = user.age
+#     context["request"] = request
+    
+#     return templates.TemplateResponse("user_detail.html", context)
+
+# @app.post("/users")
+# def create_user(users: User):
+    
+#     userlist = list(users)
+#     uname = userlist[1][1]
+#     uage = userlist[2][1]
+    
+#     user = UserTable()
+#     user.name = uname
+#     user.age = uage
+    
+#     session.add(user)
+#     session.commit()
+    
+#     return { "result_msg", f"{uname} created" }
+
+# @app.put("/users")
+# def update_users(users: User):
+    
+# 	userlist = list(users)
+# 	uid = userlist[0][1]
+# 	uname = userlist[1][1]
+# 	uage = userlist[2][1]
+ 
+# 	user = session.query(UserTable).filter(UserTable.id == uid).first()
+# 	user.name = uname
+# 	user.age = uage
+# 	session.commit()
+	
+# 	return { "result_msg", f"{users[0].name} updated" }
+
+# @app.delete("/users")
+# def delete_users(users: User):
+    
+# 	userlist = list(users)
+# 	uid = userlist[0][1]
+
+# 	user = session.query(UserTable).filter(UserTable.id == uid).first()
+# 	session.commit()
+
+# 	return { "result_msg", "User deleted" }
+
+
+
+
+# # @app.get("/")
+# # async def root():
+# # 	return {"message": "Hello world"}
+
+# # # @app.get("/items/{item_id}")
+# # # async def read_item(item_id: int):
+# # #     return {"item_id": item_id}
+
+# # @app.get("/users/me")
+# # async def read_user_me():
+# #     return {"user_id": "the current user"}
+
+# # @app.get("/users/{user_id}")
+# # async def read_user(user_id: str):
+# #     return {"user_id": user_id}
+
+# # @app.get("/models/{model_name}")
+# # async def get_model(model_name: ModelName):
+# #     if model_name is ModelName.alexnet:
+# #         return {"model_name": model_name, "message": "DeepLearning FTW!"}
+    
+# #     if model_name.value == "lenet":
+# #         return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+# #     return {"model_name": model_name, "message": "Have some residuals"}
+
+# # @app.get("/files/{file_path:path}")
+# # async def read_file(file_path: str):
+# #     return {"file_path": file_path}
+
+
+# # # --------------------------------------------------
+# # fake_items_db = [
+# # 	{"item_name": "Foo", "number": 1},
+# # 	{"item_name": "Bar", "number": 2},
+# # 	{"item_name": "Baz", "number": 3},
+# # 	{"item_name": "Foo", "number": 4},
+# # 	{"item_name": "Bar", "number": 5},
+# # 	{"item_name": "Baz", "number": 6},
+# # 	{"item_name": "Foo", "number": 7},
+# # 	{"item_name": "Bar", "number": 8},
+# # 	{"item_name": "Baz", "number": 9},
+# # 	{"item_name": "Foo", "number": 10},
+# # 	{"item_name": "Bar", "number": 11},
+# # 	{"item_name": "Baz", "number": 12},
+# # 	{"item_name": "Foo", "number": 13},
+# # 	{"item_name": "Bar", "number": 14},
+# # 	{"item_name": "Baz", "number": 15},
+# # 	{"item_name": "Foo", "number": 16},
+# # 	{"item_name": "Bar", "number": 17},
+# # 	{"item_name": "Baz", "number": 18},
+# # 	{"item_name": "Foo", "number": 19},
+# # 	{"item_name": "Bar", "number": 20},
+# # 	{"item_name": "Baz", "number": 21},
+# # 	{"item_name": "Foo", "number": 22},
+# # 	{"item_name": "Bar", "number": 23},
+# # 	{"item_name": "Baz", "number": 24},
+# # ]
+
+# # @app.get("/items/")
+# # async def read_item(skip: int = 0, limit: int = 10):
+# #     return fake_items_db[skip: skip + limit]
+
+# # # @app.get("/items/{item_id}")
+# # # async def read_item(item_id: str, q: Union[str, None] = None):
+# # #     if q:
+# # #         return {"item_id": item_id, "q": q}
+# # #     return {"item_id": item_id}
+
+# # # @app.get("/items/{item_id}")
+# # # async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+# # #     item = {"item_id": item_id}
+# # #     if q:
+# # #         item.update({"q": q})
+# # #     if not short:
+# # #         item.update(
+# # # 			{"description": "This is an amazing item that has a long description"}
+# # # 		)
+# # #     return item
+
+# # # @app.get("/users/{user_id}/items/{item_id}")
+# # # async def read_user_item(
+# # #     user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+# # # ):
+# # #     item = {"item_id": item_id, "owner_id": user_id}
+# # #     if q:
+# # #         item.update({"q": q})
+# # #     if not short:
+# # #         item.update(
+# # #             {"description": "This is an amazing item that has a long description"}
+# # #         )
+# # #     return item
+
+# # # @app.get("/items/{item_id}")
+# # # async def read_user_item(item_id: str, needy: str):
+# # #     item = {"item_id": item_id, "needy": needy}
+# # #     return item
+
+# # @app.get("/items/{item_id}")
+# # async def read_user_item(
+# # 	item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+# # ):
+# #     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+# #     return item
