9021c587333afe851014d2340aa1e45206dfd9da
Heechan Kang <weston0713@gmail.com>
2022-08-30 22:04:13 +0900

    220830

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from imp import reload
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=False)
+    uvicorn.run('app.main2:app', host='0.0.0.0', port=8000, reload=True)
