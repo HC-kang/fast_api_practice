c1c644c13a170c24b27449c3c01df670041a5846
Heechan Kang <weston0713@gmail.com>
2022-08-31 22:52:00 +0900

    220831

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from imp import reload
 import uvicorn
 
+
 if __name__ == '__main__':
-    uvicorn.run('app.main2:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('ToDoApp.main:app', host='0.0.0.0', port=8000, reload=True)
