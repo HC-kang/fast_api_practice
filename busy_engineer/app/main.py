81b6a68a91be48c9536cb930fca35b7609902845
Heechan Kang <weston0713@gmail.com>
2022-09-08 07:46:29 +0900

    220908

--- busy_engineer/app/main.py
+++ busy_engineer/app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
-@app.get("/")
-async def start():
-    return 'hi';
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """Root
+
+    Returns:
+        "msg": "Hello!!"
+    """
+    return {"msg": "Hello!!"}
+
+app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
