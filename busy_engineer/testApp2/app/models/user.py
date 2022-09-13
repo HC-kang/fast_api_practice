3d69ee1b48ddd6d01e72f075e23718aedb593e28
Heechan Kang <weston0713@gmail.com>
2022-09-14 08:16:53 +0900

    user model, schema 수정

---
+++ busy_engineer/testApp2/app/models/user.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Integer, String, Column, Boolean, Enum, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.db.base_class import Base
+from app.enums.user_approve_status_flag import UserApproveStatusFlag
+
+
+class User(Base):
+    
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(256), nullable=True)
+    hashed_password = Column(String(256), nullable=False)
+    email = Column(String(256), index=True, nullable=False)
+    
+    phone = Column(String(256), nullable=True)
+    level = Column(Integer, nullable=False)
+    role = Column(String(20), nullable=False)
+    credit_point = Column(Integer, nullable=False, default=0)
+    free_point = Column(Integer, nullable=False, default=0)
+    business_class = Column(String(256), nullable=True)
+    business_name = Column(String(256), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_notification = Column(Boolean, default=False)
+    business_president = Column(String(256), nullable=True)
+    approve_status_flag = Column(
+        Enum(UserApproveStatusFlag), default=UserApproveStatusFlag.W
+    )
+    is_superuser = Column(Boolean, default=False)
+    
+    recipes = relationship(
+        "Recipe",
+        cascade="all,delete-orphan",
+        back_populates="submitter",
+        uselist=True,
+    )
---
+++ busy_engineer/testApp2/app/schemas/user.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+from app.enums.user_approve_status_flag import UserApproveStatusFlag
+
+
+class UserBase(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    
+    phone: Optional[str] = None
+    level: int = 1
+    role: str = 'novice'
+    credit_point: int = 0
+    free_point: int = 0
+    business_class: Optional[str] = None
+    business_name: Optional[str] = None
+    is_notification: bool = False
+    business_president: Optional[str] = None
+    approve_status_flag: Enum = UserApproveStatusFlag.W
+    
+    is_superuser: bool = False
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    id: int
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties stored in DB but not returned by API
+class UserInDB(UserInDBBase):
+    hashed_password: str
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    ...
